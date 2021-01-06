58631bb527c6528057ac7d6a2a07e0b1b4b04ace
user <user>
2021-01-06 23:07:49 +0100

    working data script

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -2,25 +2,20 @@
 from collections import Counter
 import os
 import json
+import re
+import pandas as pd
 
 
 
 baseDir = "/home/user/workspaces/MasterThesis/data"
+txtDir = os.path.join(baseDir,"txt")
+
+dictResultPath = os.path.join(baseDir,"dictResult")
+if not os.path.exists(dictResultPath):
+    os.makedirs(dictResultPath)
 
 keywordsFile = "/home/user/workspaces/MasterThesis/scripts/02_ingest_transcripts/keywords.json"
 
-txtFile = os.path.join(baseDir,"txt","2005","01-11.txt")
-
-
-test = '''Herman Van Rompuy, President of the European Council. − Mr President, President of the
-European Commission, honourable Members, within the space of 49 days, I have chaired
-3 meetings of the European Council and a Summit of Heads of State and Government of
-the eurozone. These facts illustrate the great and urgent challenges our Union is facing,
-both on the economic and on the diplomatic front. It also neatly illustrates that meetings
-of the European Council are not just an event: they are part of a process. Indeed, in the'''
-
-test2 = test.split()
-print(test2)
 
 # load JSON file with categories
 with open(keywordsFile) as keywordsJSON:
@@ -38,7 +33,7 @@ def countKeywords(txtFilePath, keyWordDict):
 
     for category in keywordCategories:
         result[category] = {}
-        print("Checking keywords for category {}".format(category))
+        #print("Checking keywords for category {}".format(category))
         for word in txtSplit:
             counter[word] += 1
         f.close() 
@@ -46,7 +41,68 @@ def countKeywords(txtFilePath, keyWordDict):
         for keyword in keywords:
             #print("{}: {}".format(keyword,counter[keyword]))
             result[category][keyword] = counter[keyword]
-    print(result)
+    return result
 
 
-print(countKeywords(txtFile,keywordCategories))
+# create dataframes
+categoryResults = {}
+for category in keywordCategories:
+    columns = keywordCategories[category]
+    print(type(columns))
+    columns.append("sum")
+    columns.append("date")
+
+    df = pd.DataFrame(columns=columns)
+    #df = df.fillna(0) # with 0s rather than NaNs
+    categoryResults[category] = df
+#print(categoryResults)
+
+years = os.listdir(txtDir)
+for year in years:
+    yearPath = os.path.join(dictResultPath,year)
+    if not os.path.exists(yearPath):
+        os.makedirs(yearPath)
+    yearDir = os.path.join(txtDir,year)
+    txtFiles = os.listdir(yearDir)
+    for day in txtFiles:
+        search = re.search(r"([\d]{2}-[\d]{2})", day)
+        monthDay = search.group()
+        txtFile = os.path.join(txtDir,year,day)
+        result = countKeywords(txtFile,keywordCategories)
+        resultFile = os.path.join(dictResultPath, year, monthDay + ".json")
+
+
+        for category in keywordCategories:
+
+            df = pd.DataFrame([result[category]])
+            df['sum'] = sum(result[category].values())
+            df['date'] = year + "-" + monthDay
+            if categoryResults[category].empty:
+                categoryResults[category] = df
+            else:
+                categoryResults[category] = pd.concat([categoryResults[category],df], axis=0)
+        
+        #print(df)
+        #categoryResults[category].append(df)
+        #print(df)
+        #print(pd.concat([categoryResults[category]  , df], axis=0))
+
+
+        
+        #for category in keywordCategories:
+        #
+        # exit()
+        # if os.path.isfile(resultFile):
+        #     print ("analysis exists", end='')
+        # else:
+        #     print ("analysis writing", end='')
+        #     with open(resultFile, 'w') as f:
+        #         f.write(json.dumps(result))
+
+        print(year + "-" + monthDay)
+
+
+for category in keywordCategories:
+    resultPath = os.path.join(dictResultPath,category+".csv")
+    categoryResults[category].to_csv(resultPath)
+#print(countKeywords(txtFile,keywordCategories))
