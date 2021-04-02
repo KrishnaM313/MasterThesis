e2d3545909692fc835c636e61128b624df341e2b
Michael Bodnar <Michael.Bodnar@reeeliance.com>
2021-04-02 17:46:35 +0200

    working training and logging

--- scripts/08_cnn_train.py
+++ scripts/08_cnn_train.py
@@ -14,6 +14,10 @@ from datasets import Dataset
 import mlflow
 from azureml.core import Run
 import argparse
+import torch.nn as nn
+from sklearn.metrics import classification_report
+import json
+from tools_logging import logValues, logConfusionMatrix
 # from pytorch_lightning.loggers import MLFlowLogger
 # from torch.nn import functional as F
 
@@ -142,7 +146,7 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print("push model")
     print(torch.cuda.memory_allocated()/1024**2)
-    model.train().to(device)
+
     print(torch.cuda.memory_allocated()/1024**2)
     optimizer = torch.optim.AdamW(
         params=model.parameters(), lr=args.learning_rate)
@@ -155,18 +159,27 @@ if __name__ == '__main__':
     test_loss = 0.0
     test_count = 0
 
-    for epoch in range(args.epochs):
+    demoLimit=0
 
-                    correct = 0
-            total = 0
-            outputs_epoch = []
-            labels_epoch = []
-            predicted_epoch = []
-            running_acc = 0.
-            running_loss = 0.
+    train_criterion = nn.CrossEntropyLoss()
+
+    total_step = len(trainDataloader)
+
+    for epoch in range(args.epochs):
+        model.train().to(device)
+        correct = 0
+        total = 0
+        outputs_epoch = []
+        labels_epoch = []
+        predicted_epoch = []
+        running_acc = 0.
+        running_loss = 0.
 
         for i, batch in enumerate(tqdm(trainDataloader)):
-            print(torch.cuda.memory_allocated()/1024**2)
+            if (demoLimit>0) and (i>demoLimit):
+                break
+
+            #print(torch.cuda.memory_allocated()/1024**2)
             input_ids = batch["input_ids"]
             input_ids = input_ids.to(device)
 
@@ -180,13 +193,36 @@ if __name__ == '__main__':
             labels = labels.to(device)
 
             # del batch
-            outputs = model(
+            outputs, aux_outputs = model(
                 input_ids=input_ids,
                 token_type_ids=token_type_ids,
                 attention_mask=attention_mask,
                 labels=labels
             )  # **batch
 
+
+            _, predicted = torch.max(aux_outputs, 1)
+            #print(predicted)
+            #print(labels)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            loss = train_criterion(aux_outputs, labels)
+
+
+            # appending the overall predicted and target tensor for the whole epoch to calculate the metrics as lists
+            labels_epoch.append(labels)
+            predicted_epoch.append(predicted)
+           
+            # calculating the running loss
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (i+1)
+
+            # calculating the running accuracy
+            acc_t = 100*(correct/total)
+            running_acc += (acc_t - running_acc) / (i+1)
+
+    
             # loss = loss_fn(outputs, labels)
 
             del input_ids
@@ -194,63 +230,110 @@ if __name__ == '__main__':
             del attention_mask
             del labels
 
-            loss = outputs[0]
+            #loss = outputs[0]
             # loss = F.cross_entropy(outputs.logits, labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            if i % 10 == 0:
-                # mlf_logger.log_metric("loss", loss)
-                run.log('loss', outputs[0].item())
-                print(
-                    f"epoch={epoch + 1}, batch={i + 1:5}: loss {outputs[0].item():.2f}")
-            train_loss += outputs[0]
-            train_count += 1
+            # if i % 10 == 0:
+            #     # mlf_logger.log_metric("loss", loss)
+            #     run.log('loss', outputs[0].item())
+            #     print(
+            #         f"epoch={epoch + 1}, batch={i + 1:5}: loss {outputs[0].item():.2f}")
+
+            if (i+1) % 20 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                    .format(epoch+1, args.epochs, i+1, total_step, loss.item()))
+
+        labels_epoch = torch.cat(labels_epoch).cpu()
+        predicted_epoch = torch.cat(predicted_epoch).cpu()
+
+        # calculating the classification report with sklearn    
+        classification_report_json = classification_report(labels_epoch, predicted_epoch, output_dict=True, zero_division=0)
+#           classification_report_str = classification_report(labels_epoch, predicted_epoch, output_dict=False, zero_division=0)
+        
+        labels_epoch_str =  " ".join(str(x) for x in labels_epoch.numpy().tolist()) 
+        predicted_epoch_str = " ".join(str(x) for x in predicted_epoch.numpy().tolist())
+
+        #logConfusionMatrix(run, labels_epoch, predicted_epoch, verbose=True)
+
+
+
+        result = {
+            "epoch" : epoch,
+            "correct" : correct,
+            "total" : total,
+            "accuracy" : running_acc, 
+            "stage" : 'testing',
+            "classification_report_json" : classification_report_json,
+#               "classification_report_str" : classification_report_str,
+            "predicted_epoch": predicted_epoch_str,
+            "labels_epoch": labels_epoch_str
+        }
+        logValues(run, result)
+
+        #logConfusionMatrix(run, confusion_matrix)
+
+
+
+            #print(result)
+            #print(json.dumps(result, indent=4))
+            #print(confusion_matrix)
+
+#            print('Test Accuracy on Testing: {} %'.format(running_acc))
+#            print('F1-score Macro on Testing: {}'.format(classification_report_json['macro avg']['f1-score']))
+#            print('Precision of Hate on Testing: {}'.format(classification_report_json['0']['precision']))
+#            print('Recall of Hate on Testing: {}'.format(classification_report_json['0']['recall']))
+#            print('F1-score of Hate on Testing: {}'.format(classification_report_json['0']['f1-score']))
+        #exit()
+            #train_loss += outputs[0]
+            #train_count += 1
         torch.save(model.state_dict(), os.path.join(
             args.output_dir, "model_epoch"+str(epoch)+postfix))
-    model.eval()  # prep model for evaluation
-    for i, batch in enumerate(tqdm(testDataloader)):
-        # forward pass: compute predicted outputs by passing inputs to the model
+    # model.eval()  # prep model for evaluation
+    # for i, batch in enumerate(tqdm(testDataloader)):
+    #     # forward pass: compute predicted outputs by passing inputs to the model
 
-        input_ids = batch["input_ids"]
-        input_ids = input_ids.to(device)
+    #     input_ids = batch["input_ids"]
+    #     input_ids = input_ids.to(device)
 
-        token_type_ids = batch["token_type_ids"]
-        token_type_ids = token_type_ids.to(device)
+    #     token_type_ids = batch["token_type_ids"]
+    #     token_type_ids = token_type_ids.to(device)
 
-        attention_mask = batch["attention_mask"]
-        attention_mask = attention_mask.to(device)
+    #     attention_mask = batch["attention_mask"]
+    #     attention_mask = attention_mask.to(device)
 
-        labels = batch["labels"]
-        labels = labels.to(device)
+    #     labels = batch["labels"]
+    #     labels = labels.to(device)
 
-        outputs = model(
-            input_ids=input_ids,
-            token_type_ids=token_type_ids,
-            attention_mask=attention_mask,
-            labels=labels)
+    #     outputs = model(
+    #         input_ids=input_ids,
+    #         token_type_ids=token_type_ids,
+    #         attention_mask=attention_mask,
+    #         labels=labels)
 
-        # calculate the loss
-        # print(outputs)
-        # loss =
-        # loss = loss_fn(output, labels)
+    #     # calculate the loss
+    #     # print(outputs)
+    #     # loss =
+    #     # loss = loss_fn(output, labels)
 
-        del input_ids
-        del token_type_ids
-        del attention_mask
-        del labels
+    #     del input_ids
+    #     del token_type_ids
+    #     del attention_mask
+    #     del labels
 
-        # update running validation loss
-        test_loss += outputs[0]
-        test_count += 1
+    #     # update running validation loss
+    #     test_loss += outputs[0]
+    #     test_count += 1
 
-    # print training/validation statistics
-    # calculate average loss over an epoch
-    train_loss = train_loss/train_count  # len(trainDataloader)
-    run.log('avg_train_loss', train_loss.item())
-    test_loss = test_loss/test_count  # len(testDataloader)
-    run.log('avg_test_loss', test_loss.item())
+    # # print training/validation statistics
+    # # calculate average loss over an epoch
+    # train_loss = train_loss/train_count  # len(trainDataloader)
+    # run.log('avg_train_loss', train_loss.item())
+    # test_loss = test_loss/test_count  # len(testDataloader)
+    # run.log('avg_test_loss', test_loss.item())
+    
     # model.eval()
     # y_pred = model(testX)
     # test_loss = criterion(y_pred, testY)
---
+++ scripts/tools_logging.py
@@ -0,0 +1,49 @@
+from sklearn.metrics import multilabel_confusion_matrix
+
+def logValues(run, result: dict):
+    # result = {
+    #     "epoch" : epoch,
+    #     "correct" : correct,
+    #     "total" : total,
+    #     "accuracy" : running_acc, 
+    #     "stage" : 'testing',
+    #     "classification_report_json" : classification_report_json,
+    #     "predicted_epoch": predicted_epoch_str,
+    #     "labels_epoch": labels_epoch_str
+    # }
+    keys = ["epoch", "correct", "total", "accuracy"]
+    for key in keys:
+        run.log(key, result[key])
+
+
+
+def logConfusionMatrix(run, labels_epoch, predicted_epoch, labels=None, labelsRange=9, verbose=False):
+    if verbose:
+        print(labels_epoch)
+        print(predicted_epoch)
+
+    if labels is None:
+        labels = [int(x) for x in range(labelsRange)]
+    
+    if verbose:
+        print(labels)
+
+    confusion_matrices = multilabel_confusion_matrix(labels_epoch, predicted_epoch, labels=labels)
+
+    for i, label in enumerate(labels):
+        if verbose:
+            print("Label #%s %s" %(i,label))
+        confusion_matrix = confusion_matrices[i]
+        logJSON = {
+            "schema_type": "confusion_matrix",
+            "schema_version": "1.0.0",
+            "data": {
+                "class_labels": labels,
+                "matrix": [[int(y) for y in x] for x in confusion_matrix]
+            }
+        }
+        print(logJSON)
+        run.log_confusion_matrix('confusion_matrix_' + label, logJSON)
+
+    
+    
